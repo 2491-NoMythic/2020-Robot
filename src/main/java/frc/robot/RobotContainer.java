aa4998359e6dfcea475ad963a387cd9545ec9eec
subearthling <hmaclaughlin@greatriverschool.org>
2020-01-25 13:27:15 -0600

    Commented out getAutonomousCommand method
    the method is null at the moment so all it does is throw an error.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -58,8 +58,10 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
+  /**
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return m_autoCommand;
   }
+  */
 }
