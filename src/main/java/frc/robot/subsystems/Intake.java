56e0b48787e6c1ec423e934010c2d46282142170
Maxim Taulbee Testing <2491Nomythic@gmail.com>
2020-01-28 17:24:03 -0600

    changed intake to use constants

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,33 +10,34 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-  Talon CoreMotor;
+  Talon CoreIntakeMotor;
   Solenoid IntakeSolenoid;
   /**
    * Creates a new Intake.
    */
   public Intake() {
-    CoreMotor = new Talon(2);
-    IntakeSolenoid = new Solenoid(2491);
+    CoreIntakeMotor = new Talon(Constants.intakeMotorPort);
+    IntakeSolenoid = new Solenoid(Constants.intakeSolenoidPort);
     IntakeSolenoid.set(false);
   }
   
-  public void StartMotor(double motorPercent) {
-    CoreMotor.set(motorPercent);
+  public void StartIntakeMotor(double motorPercent) {
+    CoreIntakeMotor.set(motorPercent);
   }
 
-  public void StopMotor() {
-    CoreMotor.set(0);
+  public void StopIntakeMotor() {
+    CoreIntakeMotor.set(0);
   }
 
-  public void toggleSolenoid() {
+  public void toggleIntakeSolenoid() {
     boolean a = !IntakeSolenoid.get();
     IntakeSolenoid.set(a);
   }
 
-  public boolean checkSolenoid() {
+  public boolean checkIntakeSolenoid() {
     return IntakeSolenoid.get();
   }
   @Override
