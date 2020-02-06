a2b2595b59f28748e89317e23768a840b790d400
Mara <mgould@avalonschool.org>
2020-02-06 17:55:16 -0600

    Added Climber

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Settings.Constants;
+
+public class Climber extends SubsystemBase {
+  TalonSRX lift;
+
+  /**
+   * Creates a new Climber.
+   */
+  public Climber() {
+    lift = new TalonSRX(Constants.Climber.liftMotorID);
+
+    lift.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+  }
+
+  public void runLift(double speed) {
+    lift.set(ControlMode.Position, speed);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
