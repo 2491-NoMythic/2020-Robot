853a4d224b5083b07b26b5b497e8c91b0e9f54d9
Elias Rojas Collins <eliasrc2002@outlook.com>
2020-02-15 09:53:24 -0600

    Fixed the timer

--- src/main/java/frc/robot/commands/timeDrive.java
+++ src/main/java/frc/robot/commands/timeDrive.java
@@ -9,6 +9,7 @@ package frc.robot.commands;
 
 import com.ctre.phoenix.time.StopWatch;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
@@ -16,24 +17,24 @@ public class timeDrive extends CommandBase {
   /**
    * Creates a new timeDrive.
    */
-  Drivetrain TrainLivesToDrive;
+  Drivetrain m_drivetrain;
   double speedOfDrive;
   double timeOfDrive;
-  StopWatch activeTimer;
+  Timer activeTimer;
   boolean timeDriveFinished;
   public timeDrive(Drivetrain driveTransfer, double speedDriveTransfer, double timeTransfer) {
-    TrainLivesToDrive = driveTransfer;
-    addRequirements(TrainLivesToDrive);
+    m_drivetrain = driveTransfer;
+    addRequirements(m_drivetrain);
     speedOfDrive = speedDriveTransfer;
     timeOfDrive = timeTransfer;
-    activeTimer = new StopWatch();
+    activeTimer = new Timer();
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    TrainLivesToDrive.drivePercentOutput(speedOfDrive);
+    m_drivetrain.drivePercentOutput(speedOfDrive);
     activeTimer.start();
   }
 
@@ -45,12 +46,12 @@ public class timeDrive extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    TrainLivesToDrive.stop();
+    m_drivetrain.stop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return timeOfDrive < activeTimer.getDuration();
+    return timeOfDrive <= activeTimer.get();
   }
 }
